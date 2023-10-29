ec2506a6fc812ccfa284af2d077f96a4efbf12e2
optik9 <choyoss@gmail.com>
2023-10-28 23:17:06 -0500

    first version

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe/screens/create_room_screen.dart';
+import 'package:tictactoe/screens/join_room_screen.dart';
+import 'package:tictactoe/screens/main_menu_screen.dart';
+import 'package:tictactoe/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor, 
+      ),
+      routes: {
+        MainMenuScreen.routeName: (context) => const MainMenuScreen(),
+        JoinRoomScreen.routeName:(context) => const JoinRoomScreen(),
+        CreateRoomScreen.routeName:(context) => const CreateRoomScreen(),
+      },
+      initialRoute: MainMenuScreen.routeName,
+    );
+  }
+}
+
