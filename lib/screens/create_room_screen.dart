711bccc8b58e6e3697866be9d22e03936068ae6a
optik9 <choyoss@gmail.com>
2023-11-01 19:36:18 -0500

    add screen create room

--- lib/screens/create_room_screen.dart
+++ lib/screens/create_room_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tictactoe/responsive/responsive.dart';
+import 'package:tictactoe/widgets/custom_button.dart';
+import 'package:tictactoe/widgets/custom_text.dart';
+import 'package:tictactoe/widgets/custom_textfield.dart';
 
 class CreateRoomScreen extends StatefulWidget {
   static String routeName = '/create-room';
@@ -9,9 +13,57 @@ class CreateRoomScreen extends StatefulWidget {
 }
 
 class _CreateRoomScreenState extends State<CreateRoomScreen> {
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _nameController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+        
+      
+        
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const CustomText(
+          shadows: [
+            Shadow(
+              blurRadius: 40,
+              color: Colors.blue,
+            ),
+          ],
+          text: 'Create Room',
+          fontSize: 70,
+          ),
+          SizedBox(height: size.height * 0.08),
+          CustomTextField(
+            controller: _nameController, 
+            hintText: 'Enter your nickname',
+            ),
+      
+          SizedBox(height: size.height * 0.04),
+          CustomButton (onTap: () {}, text: 'Create'),
+
+      ],
+      ),
+      ),
+      ),
+
+
+    );
 
     //comment
   }
